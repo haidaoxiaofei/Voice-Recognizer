397ecc1b992c1bfc9442e80f8090ad7c9275c0aa
Peng CHENG <haidaoxiaofei@gmail.com>
2014-06-19 16:44:24 +0800

    Record WAV format file.

---
+++ src/hk/ust/cse/utils/AppLog.java
@@ -0,0 +1,11 @@
+package hk.ust.cse.utils;
+
+import android.util.Log;
+
+public class AppLog {
+	private static final String APP_TAG = "AudioRecorder";
+    
+    public static int logString(String message){
+            return Log.i(APP_TAG,message);
+    }
+}
